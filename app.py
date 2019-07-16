efe9bb1e2498ecd60266486269d629bdc277e15c
calmconquerer <wasiullah.khan21@gmail.com>
2019-07-16 19:29:23 +0500

    fixed  a couple of issues and added a requirements.txt file

--- app.py
+++ app.py
@@ -17,7 +17,11 @@ def navigation():
     driver.find_element(By.CSS_SELECTOR, 'body > app-root > div:nth-child(1) > app-base > mat-sidenav-container > mat-sidenav-content > app-product > div > div:nth-child(3) > div.productInfo > div.pt-2.ng-star-inserted > div.row > div:nth-child(2) > button').click()
     driver.find_element(By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav/kfc-cart/div/div[2]/div/div/mat-form-field[1]/div/div[1]').click()
     time.sleep(5)
-    # driver.find_element(By.ID, 'mat-option-150').click()
+    driver.find_element(By.XPATH, '//*[@id="mat-option-7"]').click()
+    driver.find_element(By.XPATH, '//*[@id="mat-select-1"]/div/div[1]').click()
+    driver.find_element(By.XPATH, '//*[@id="mat-option-58"]').click()
+    driver.find_element(By.XPATH, '//*[@id="mat-select-2"]/div/div[1]').click()
+
 
 
 try:
