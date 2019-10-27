0a10fc0fa828b9b50d2de6bba71c94341b4df716
calmconquerer <wasiullah.khan21@gmail.com>
2019-10-27 18:31:08 +0500

    Developed the KFC-ORDER automation bot

--- app.py
+++ app.py
@@ -6,22 +6,69 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import config
 
+# options = webdriver.FirefoxOptions("--headless")
+
 driver = webdriver.Firefox()
 
 
 def navigation():
     driver.get('https://www.kfcpakistan.com')
-    driver.find_element(By.XPATH, '(//*[@id="dropdownTest"])[3]').click()
-    driver.implicitly_wait(5)
-    driver.find_element(By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/app-collections/div/div[2]/div/div/div/product-card[3]/div/div[2]/div[2]/button').click()
-    driver.find_element(By.CSS_SELECTOR, 'body > app-root > div:nth-child(1) > app-base > mat-sidenav-container > mat-sidenav-content > app-product > div > div:nth-child(3) > div.productInfo > div.pt-2.ng-star-inserted > div.row > div:nth-child(2) > button').click()
-    driver.find_element(By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav/kfc-cart/div/div[2]/div/div/mat-form-field[1]/div/div[1]').click()
-    time.sleep(5)
-    driver.find_element(By.XPATH, '//*[@id="mat-option-7"]').click()
-    driver.find_element(By.XPATH, '//*[@id="mat-select-1"]/div/div[1]').click()
-    driver.find_element(By.XPATH, '//*[@id="mat-option-58"]').click()
-    driver.find_element(By.XPATH, '//*[@id="mat-select-2"]/div/div[1]').click()
+    WebDriverWait(driver, 1000).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/app-header/header/div/div[2]/app-nav/div/div[3]")))
+    driver.find_element(By.XPATH,
+                        '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/app-header/header/div/div[2]/app-nav/div/div[3]').click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-collections/div/div[2]/div/div/div/product-card[3]/div/div[2]/div[2]/button')))
+    wow_box = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-collections/div/div[2]/div/div/div/product-card[3]/div/div[2]/div[2]/button")
+    wow_box.click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-product/div/div[2]/div[2]/div[3]/div[2]/div[2]/button')))
+    add_to_bucket = driver.find_element(By.XPATH, '/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-product/div/div[2]/div[2]/div[3]/div[2]/div[2]/button')
+    add_to_bucket.click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.CLASS_NAME, "mat-select-value")))
+    city_option = driver.find_element(By.CLASS_NAME, "mat-select-value")
+    city_option.click()
 
+    islamabad = driver.find_element(By.XPATH, '//*[@id="mat-option-7"]')
+    islamabad.click()
+    address = driver.find_element(By.XPATH, '//*[@id="mat-input-1"]')
+    address.send_keys("I-10/4")
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-option-28"]')))
+    driver.find_element(By.XPATH, '//*[@id="mat-option-28"]').click()
+    next = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav/kfc-cart/div/div[2]/div/div/button")
+    next.click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav/kfc-cart/div/div[5]/button")))
+    checkout = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav/kfc-cart/div/div[5]/button")
+    checkout.click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="frmEmailA"]')))
+    # check_out_as_guest = driver.find_element(By.XPATH,
+    #                                          "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-checkout/div/div[2]/div[2]/div[1]/div[1]/mat-card/form/mat-card-content[1]/mat-button-toggle-group/mat-button-toggle[2]/label/div")
+    # check_out_as_guest.click()
+    email_input = driver.find_element(By.XPATH, '//*[@id="frmEmailA"]')
+    email_input.send_keys("wasiullah.khan21@gmail.com")
+
+    continue_as_guest = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-checkout/div/div[2]/div[2]/div[1]/div[1]/mat-card/form/mat-card-content[3]/button")
+    continue_as_guest.click()
+
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable, '//*[@id="frmNameA"]')
+
+    full_name = driver.find_element(By.XPATH, '//*[@id="frmNameA"]')
+    full_name.send_keys("WasiUllah Khan")
+
+    address_input = driver.find_element(By.XPATH, '//*[@id="frmAddressS"]')
+    address_input.send_keys("House # 841, main service road, I-10/4")
+
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="mat-input-5"]')))
+    phone_input = driver.find_element(By.XPATH, '//*[@id="mat-input-5"]')
+    phone_input.send_keys("3325799477")
+    time.sleep(5)
+
+    continue_to_payment = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-checkout/div/div[2]/div[2]/div[1]/div[2]/form/mat-card/mat-card-content[2]/button")
+    continue_to_payment.click()
+
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable, '//*[@id="mat-radio-9"]')
+    cash_on_delivery = driver.find_element(By.XPATH, '//*[@id="mat-radio-9"]')
+    cash_on_delivery.click()
+    time.sleep(5)
+    place_order = driver.find_element(By.XPATH, "/html/body/app-root/div[1]/app-base/mat-sidenav-container/mat-sidenav-content/div/app-checkout/div/div[2]/div[2]/div[1]/div[3]/form/div/div/mat-card/mat-card-content/button")
+    place_order.click()
 
 
 try:
